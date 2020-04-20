6c931ddc40c8b7f46e8f40f181956bb8896e0437
TIOZAO <marco_aurelioo@yahoo.com.br>
2020-04-20 00:13:36 -0300

    ajustes

---
+++ js/radars/radarData.js
@@ -0,0 +1,89 @@
+//This is the title for your window tab, and your Radar
+document.title = "Tec Radar - Marco Aurelio (2019) ";
+
+
+//This is the concentic circles that want on your radar
+var radar_arcs = [
+     {'r':100,'name':'Recomendo - Conheço'}
+    ,{'r':200,'name':'Estudando'}
+    ,{'r':300,'name':'Curioso (fila de estudo)'}
+    ,{'r':400,'name':'Não irei me aprofundar'}
+    // ,{'r':500,'name':'Possible Extra if you want it'}
+ ];
+
+//This is your raw data
+//
+// Key
+//
+// movement:
+// t = moved
+// c = stayed put
+//
+// blipSize: 
+// intValue; This is optional, if you omit this property, then your blip will be size 70.
+// This give you the ability to be able to indicate information by blip size too
+//
+// url:
+// StringValue : This is optional, If you add it then your blips will be clickable to some URL
+//
+//"pc":polar coordinates
+// r = distance away from origin ("radial coordinate")
+// - Each level is 100 points away from origin
+// t = angle of the point from origin ("angular coordinate")
+// - 0 degrees is due east
+//
+// Coarse-grained quadrants
+// - Techniques: elements of a software development process, such as experience design; and ways of structuring software, such micro-services.
+// - Tools: components, such as databases, software development tools, such as versions control systems; or more generic categories of tools, such as the notion of polyglot persistance.
+// - Platforms: things that we build software on top of: mobile technologies like Android, virtual platforms like the JVM, or generic kinds of platforms like hybrid clouds
+// - Programming Languages and Frameworks
+//
+// Rings:
+// - Adopt: blips you should be using now; proven and mature for use
+// - Trial: blips ready for use, but not as completely proven as those in the adopt ring; use on a trial basis, to decide whether they should be part of your toolkit
+// - Assess: things that you should look at closely, but not necessarily trial yet - unless you think they would be a particularly good fit for you
+// - Hold: things that are getting attention in the industry, but not ready for use; sometimes they are not mature enough yet, sometimes they are irredeemably flawed
+// Note: there's no "avoid" ring, but throw things in the hold ring that people shouldn't use.
+
+var h = 1000;
+var w = 1200;
+
+var radar_data = [
+    { "quadrant": "Techniques",
+    "left" : 45,
+    "top" : 18,
+    "color" : "#8FA227",
+    "items" : [ 
+        
+    ]
+    },
+    { "quadrant": "Tools",
+    "left": w-200+30,
+    "top" : 18,
+    "color" : "#587486",
+    "items" : [ 
+        
+    ]
+    },
+    { "quadrant": "Platforms",
+    "left" :45,
+    "top" : (h/2 + 18),
+    "color" : "#DC6F1D",
+    "items" : [
+        {"name":"AWS", "pc":{"r":130,"t":260},"movement":"t"}, 
+    ]
+    },
+    { "quadrant": "Languages & Frameworks",
+    "color" : "#B70062",
+    "left" : (w-200+30),
+    "top" : (h/2 + 18),
+    "items" : [ 
+        {"name":'Java'  ,"pc":{"r": 80 ,"t": 300 }, "movement":'t', "url":"#" },
+        {"name":'NodeJs',"pc":{"r": 130 ,"t": 320 }, "movement":'c', "over":"ok"},
+        {"name":'GoLang',"pc":{"r": 150,"t": 320 }, "movement":'c' },
+        {"name":'Spring',"pc":{"r": 60 ,"t": 290 }, "movement":'c' },
+        {"name":'Spring-Boot',"pc":{"r": 50 ,"t": 320 }, "movement":'c' }
+        
+    ]
+    }
+];
